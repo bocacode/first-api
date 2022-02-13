a35b3f152e146d170890313552a4e7377eff568f
Christian John William Klein <cjwk@Christians-MacBook-Air.local>
2022-02-13 09:34:25 -0500

    test

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const express = require('express');
-const { initializeApp, getApps, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-const credentials = require('./credentials.json');
+const express = require("express");
+const { initializeApp, getApps, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const credentials = require("./credentials.json");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 function connectToFirestore() {
-  if(!getApps().length) {
+  if (!getApps().length) {
     initializeApp({
-      credential: cert(credentials)
+      credential: cert(credentials),
     });
   }
   return getFirestore();
 }
 
-
-app.get('/restaurants', (req, res) => {
+app.get("/restaurants", (req, res) => {
   const db = connectToFirestore();
-  db.collection('restaurants').get()
-    .then(snapshot => {
-      const restaurants = snapshot.docs.map(doc => {
+  db.collection("restaurants")
+    .get()
+    .then((snapshot) => {
+      const restaurants = snapshot.docs.map((doc) => {
         let restaurant = doc.data();
         restaurant.id = doc.id;
-        return restaurant
+        return restaurant;
       });
       res.status(200).send(restaurants);
     })
     .catch(console.error);
-})
-
-app.get('/', (request, response) => {
-  response.send('My first Express API... works!');
 });
 
-app.get('/test', (request, response) => {
-  response.send('✅✅✅ The Test passed!!!');
+app.get("/", (request, response) => {
+  response.send("My first Express API... works!");
+});
+
+app.get("/test", (request, response) => {
+  response.send("✅✅✅ The Test passed!!!");
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
